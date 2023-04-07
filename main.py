adce9a1bec4a9c45de1b6c1497f50e0696880e3a
Alex Gisbert Lizaga <a.gisbertl.2017@alumnos.urjc.es>
2023-04-07 20:53:10 +0200

    Arreglo función

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ import string
 i, j=0, 0
 files=[[],[],[],[]]
 percentaje=[]
-for directory in os.listdir("./resources/dataset"):
-    j=0
-    for file in os.listdir("./resources/dataset/"+directory):
-        files[i].append(file)
-        j+=1
-    percentaje.append(j)
-    i+=1
+
 diccionario=""
 with open("./resources/diccionario.txt", encoding="utf-8") as dic:
     diccionario=dic.read()
@@ -27,22 +21,25 @@ res2=0
 string_list=string_final.split("\n")
 i=0
 for directory in os.listdir("./resources/dataset"):
-    for j in files[i]:
-        if(j is ("*.txt" or "*.csv" or "*.json" or "*.xls*" or "*.word*")):
-            with open("./resources/dataset/"+directory+"/"+j) as f:
-                string_temp=f.read()
+    for file in os.listdir("./resources/dataset/"+directory):
+        if file.endswith(".txt") or file.endswith(".json") or file.endswith(".csv"):
+            with open("./resources/dataset/" + directory + "/" + file, encoding="utf-8") as f:
+                string_temp = f.read()
             for z in string_list:
-                if(z in string_temp):
-                    res1+=1
+                if (z in string_temp):
+                    res1 += 1
                 else:
-                    res1-=1
-            if(res1>0):
-                res2+=1
-            res1=0
+                    res1 -= 1
+            if (res1 > 0):
+                res2 += 1
+
+            print(f"({file}, {res1}, {directory})")
+            res1 = 0
     result.append(res2)
-    res2=0
-    i+=1
+    res2 = 0
+    percentaje.append(j)
 print(result)
+
 
 
 
