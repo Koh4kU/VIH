1b28660321323144edbe676fab1b79d7d6eaa11f
Alex Gisbert Lizaga <a.gisbertl.2017@alumnos.urjc.es>
2023-04-08 14:22:32 +0200

    actualizado funcion

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 i, j=0, 0
 files=[[],[],[],[]]
 percentaje=[]
-
+total=[]
 diccionario=""
 with open("./resources/diccionario.txt", encoding="utf-8") as dic:
     diccionario=dic.read()
@@ -21,23 +21,29 @@ res2=0
 string_list=string_final.split("\n")
 i=0
 for directory in os.listdir("./resources/dataset"):
-    for file in os.listdir("./resources/dataset/"+directory):
+    directories=os.listdir("./resources/dataset/"+directory)
+    total.append(len(directories))
+    for file in directories:
         if file.endswith(".txt") or file.endswith(".json") or file.endswith(".csv"):
+
             with open("./resources/dataset/" + directory + "/" + file, encoding="utf-8") as f:
                 string_temp = f.read()
             for z in string_list:
                 if (z in string_temp):
                     res1 += 1
-            if (res1 > 0):
+            if (res1 >= 10):
                 res2 += 1
 
-            print(f"({file}, {res1}, {directory})")
+            #print(f"({file}, {res1}, {directory})")
             res1 = 0
     result.append(res2)
     res2 = 0
-    percentaje.append(j)
-print(result)
+print("Con VIH: "+ str(result))
+print("Total arhcivos:" +str(total))
 
+for i in range(0,4):
+    percentaje.append("%.2f" % ((result[i] * 100) / total[i]))
+print("Porcentaje: "+str(percentaje))
 
 
 
