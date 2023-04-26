d5e09688d279246b752bd68a1528b4b0b762fd78
Alex Gisbert Lizaga <a.gisbertl.2017@alumnos.urjc.es>
2023-04-26 19:53:49 +0200

    More resources baseline3

--- resources/value_script.py
+++ resources/value_script.py
@@ -2,7 +2,7 @@ with open("values.txt") as f:
     s=f.read()
 replace=s.replace(",", ".")
 print(replace)
-with open("sintomas_definitorios", encoding="Utf-8") as vih:
+with open("enfermedades_sexuales", encoding="Utf-8") as vih:
     s=vih.read()
 
 split=s.split("\n")
@@ -12,5 +12,5 @@ for i in range(len(split)):
     final = final + split[i] + "\n" + split2[i] + "\n"
 
 
-with open("sintomas_definitoriosF", "w", encoding="Utf-8") as write:
+with open("enfermedades_sexualesF", "w", encoding="Utf-8") as write:
     write.write(final)
